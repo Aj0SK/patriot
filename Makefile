f7cb61e16ac8356ee6b4368928fb4c5011219168
Andrej Korman <andrejkorman@gmail.com>
2023-08-06 19:04:24 +0200

    Move to SDL2

---
+++ Makefile
@@ -0,0 +1,20 @@
+# https://devhints.io/makefile
+CXX = g++
+CXXFLAGS = -Wall -Wextra -O2
+SDL_FLAGS = -lSDL2
+SRC = src
+BUILD = build
+
+all: run
+
+run: app
+	$(BUILD)/$<.out
+
+clean:
+	rm -r -f $(BUILD)
+
+prepare:
+	mkdir -p $(BUILD)
+
+app: prepare $(SRC)/main.cpp
+	$(CXX) $(CXXFLAGS) $(DEBUGFLAGS) -o $(BUILD)/app.out $(SRC)/main.cpp $(SDL_FLAGS)
--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,55 @@
+#include <SDL2/SDL.h>
 
-#include "Sketch.h"
+#include <vector>
 
-namespace chr
-{
-  CrossSketch* createSketch()
-  {
-    return new Sketch();
-  }
-}
+int main(int argc, char** argv) {
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Window* window =
+      SDL_CreateWindow("SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                       800, 600, SDL_WINDOW_SHOWN);
+  SDL_Renderer* renderer = SDL_CreateRenderer(
+      window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-#if defined(CHR_PLATFORM_DESKTOP)
+  const std::vector<SDL_Vertex> verts = {
+      {
+          SDL_FPoint{400, 150},
+          SDL_Color{255, 0, 0, 255},
+          SDL_FPoint{0},
+      },
+      {
+          SDL_FPoint{200, 450},
+          SDL_Color{0, 0, 255, 255},
+          SDL_FPoint{0},
+      },
+      {
+          SDL_FPoint{600, 450},
+          SDL_Color{0, 255, 0, 255},
+          SDL_FPoint{0},
+      },
+  };
 
-  int main(int argc, char** argv)
-  {
-    chr::CrossSketch::run(1024, 768, 4);
-    return 0;
-  }
-
-#elif defined(CHR_PLATFORM_EMSCRIPTEN)
-
-  int main(int argc, char** argv)
-  {
-    chr::CrossSketch::run(4);
-    return 0;
-  }
-
-#elif defined(CHR_PLATFORM_IOS)
-
-  #include "ios/Application.h"
-
-  int main(int argc, char** argv)
-  {
-    @autoreleasepool
-    {
-      return UIApplicationMain(argc, argv, nil, NSStringFromClass([Application class]));
+  bool running = true;
+  while (running) {
+    SDL_Event ev;
+    while (SDL_PollEvent(&ev)) {
+      if ((SDL_QUIT == ev.type) ||
+          (SDL_KEYDOWN == ev.type &&
+           SDL_SCANCODE_ESCAPE == ev.key.keysym.scancode)) {
+        running = false;
+        break;
+      }
     }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderGeometry(renderer, nullptr, verts.data(), verts.size(), nullptr,
+                       0);
+    SDL_RenderPresent(renderer);
   }
 
-#endif
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
+}
