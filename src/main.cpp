9b4d54d15598d0d5111d0f95a1d752a7f4700af1
Andrej Korman <andrejkorman@gmail.com>
2023-08-06 21:29:16 +0200

    Rockets are falling

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <SDL2/SDL.h>
 
 #include <chrono>
+#include <glm/glm.hpp>
 #include <vector>
 
 constexpr std::chrono::duration UPDATE_DURATION = std::chrono::milliseconds(10);
 
 struct Rocket {
   float x, y;
+  float speed;
+  glm::vec2 heading;
 };
 
 std::vector<SDL_Vertex> get_rocket_triangle(const Rocket& r) {
@@ -31,63 +34,91 @@ std::vector<SDL_Vertex> get_rocket_triangle(const Rocket& r) {
   return {a, b, c};
 }
 
+class RocketWorld {
+ public:
+  RocketWorld(int rocket_count) {
+    rockets_.reserve(rocket_count);
+
+    for (int i = 0; i < 100; ++i) {
+      const float x = static_cast<float>(rand() % 800);
+      const float y = static_cast<float>(rand() % 600);
+      rockets_.push_back(Rocket{x, y, 1});
+    }
+  }
+
+  std::vector<Rocket>& get_mutable_rockets() { return rockets_; }
+  size_t get_rocket_count() const { return rockets_.size(); }
+
+ private:
+  std::vector<Rocket> rockets_;
+};
+
+class SDLWrapper {
+ public:
+  static SDLWrapper Create() {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* window =
+        SDL_CreateWindow("SDL", SDL_WINDOWPOS_UNDEFINED,
+                         SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    return SDLWrapper(window, renderer);
+  }
+
+  SDLWrapper(SDL_Window* window, SDL_Renderer* renderer)
+      : window_(window),
+        renderer_(renderer),
+        last_update_(std::chrono::_V2::system_clock::duration::min()),
+        physics_world_(20) {}
+
+  ~SDLWrapper() {
+    SDL_DestroyRenderer(renderer_);
+    SDL_DestroyWindow(window_);
+    SDL_Quit();
+  }
+
+  void render() {
+    bool running = true;
+    while (running) {
+      SDL_Event ev;
+      while (SDL_PollEvent(&ev)) {
+        if ((SDL_QUIT == ev.type) ||
+            (SDL_KEYDOWN == ev.type &&
+             SDL_SCANCODE_ESCAPE == ev.key.keysym.scancode)) {
+          running = false;
+          break;
+        }
+      }
+
+      std::vector<SDL_Vertex> vertices;
+      vertices.reserve(3 * physics_world_.get_rocket_count());
+      for (Rocket& r : physics_world_.get_mutable_rockets()) {
+        r.y += r.speed;
+        for (SDL_Vertex v : get_rocket_triangle(r)) vertices.push_back(v);
+      }
+
+      last_update_ = std::chrono::system_clock::now();
+      SDL_SetRenderDrawColor(renderer_, 0, 0, 0, SDL_ALPHA_OPAQUE);
+      SDL_RenderClear(renderer_);
+      SDL_RenderGeometry(renderer_, nullptr, vertices.data(), vertices.size(),
+                         nullptr, 0);
+      SDL_RenderPresent(renderer_);
+    }
+  }
+
+ private:
+  SDL_Window* window_;
+  SDL_Renderer* renderer_;
+  std::chrono::_V2::system_clock::time_point last_update_;
+
+  // physic world part
+  RocketWorld physics_world_;
+};
+
 int main() {
-  SDL_Init(SDL_INIT_EVERYTHING);
-  SDL_Window* window =
-      SDL_CreateWindow("SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                       800, 600, SDL_WINDOW_SHOWN);
-  SDL_Renderer* renderer = SDL_CreateRenderer(
-      window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-
   srand(2104);
-
-  auto last_update = std::chrono::system_clock::now();
-
-  float mult = 0.0;
-
-  std::vector<SDL_Vertex> verts;
-  verts.reserve(3 * 100);
-
-  for (int i = 0; i < 100; ++i) {
-    const float x = static_cast<float>(rand() % 800);
-    const float y = static_cast<float>(rand() % 600);
-    Rocket r{x, y};
-    auto to_add = get_rocket_triangle(r);
-    for (SDL_Vertex v : to_add) verts.push_back(v);
-  }
-
-  bool running = true;
-  while (running) {
-    SDL_Event ev;
-    while (SDL_PollEvent(&ev)) {
-      if ((SDL_QUIT == ev.type) ||
-          (SDL_KEYDOWN == ev.type &&
-           SDL_SCANCODE_ESCAPE == ev.key.keysym.scancode)) {
-        running = false;
-        break;
-      }
-    }
-
-    if (auto now = std::chrono::system_clock::now();
-        now - last_update > UPDATE_DURATION) {
-      for (SDL_Vertex& vertex : verts) {
-        vertex.position.y += mult;
-      }
-      const float dt = 1.0 / 100.0;
-      mult += 2 * dt * 10.0;
-      last_update = now;
-    }
-
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderGeometry(renderer, nullptr, verts.data(), verts.size(), nullptr,
-                       0);
-    SDL_RenderPresent(renderer);
-  }
-
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-
+  SDLWrapper sdl_program = SDLWrapper::Create();
+  sdl_program.render();
   return 0;
 }
